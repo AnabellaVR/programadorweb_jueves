7d2f53f0ad6decf0d34b49e748fccb7862045447
Anabella Romero <anabella_vr@hotmail.com>
2025-06-07 09:37:45 -0300

    proyecto_utn

--- proyecto_utn/ejercicio.sql
+++ proyecto_utn/ejercicio.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 06-06-2025 a las 16:40:54
+-- Tiempo de generación: 07-06-2025 a las 12:34:42
 -- Versión del servidor: 9.1.0
 -- Versión de PHP: 8.3.14
 
@@ -42,7 +42,6 @@ CREATE TABLE IF NOT EXISTS `almohadones` (
 INSERT INTO `almohadones` (`id`, `titulo`, `cuerpo`) VALUES
 (1, 'Almohadón \"Aurum Helénico\"', 'Un dúo de almohadones que combina el lujo del negro con el esplendor del dorado, inspirados en la elegancia de la Grecia clásica. El detalle central en pasamanería griega con flecos otorga un aire de distinción y carácter, ideal para ambientes sobrios con acentos de sofisticación. Perfectos para realzar un sillón de cuero, una cama neutra o un rincón de lectura con impronta clásica y atemporal.'),
 (2, 'Almohadón \"Estocolmo\"', 'Diseño minimalista con franjas en blanco, gris, dorado y negro. Sofisticado y moderno, ideal para ambientes nórdicos o urbanos con estilo.'),
-(3, 'Almohadón \"Aurum Helénico\"', 'Un dúo de almohadones que combina el lujo del negro con el esplendor del dorado, inspirados en la elegancia de la Grecia clásica. El detalle central en pasamanería griega con flecos otorga un aire de distinción y carácter, ideal para ambientes sobrios con acentos de sofisticación. Perfectos para realzar un sillón de cuero, una cama neutra o un rincón de lectura con impronta clásica y atemporal.'),
 (4, 'Almohadón \"Estocolmo\"', 'Diseño minimalista con franjas en blanco, gris, dorado y negro. Sofisticado y moderno, ideal para ambientes nórdicos o urbanos con estilo.'),
 (5, 'Almohadón \"Esmeralda\"', 'Almohadón de terciopelo verde profundo con costuras en diagonal y una franja dorada central. Elegante y vibrante, perfecto para sumar lujo y textura.'),
 (6, 'Almohadón \"Perla Zigzag, Flor de Nieve, Arabesco\"', ' Diseño en chevron bordado con cuentas. Elegante, delicado y perfecto para dar un toque glam a camas o sillones en tonos neutros.\r\nAlmohadón con flores en relieve suave y etéreo su textura aporta calidez visual, perfecta para combinar. '),
